3b91072bd6718e13edb36700431079e837a8e041
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2026-02-12 18:36:26 +0200

    aadde dependacies

---
+++ quick-ticket/app/page.tsx
@@ -0,0 +1,8 @@
+
+const HomePage = () => {
+  return ( 
+    <h1 className="text-4xl text-indigo-300">Welcome to the Home Page</h1>
+   );
+}
+ 
+export default HomePage;
---
+++ quick-ticket/sentry.edge.config.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://bcb22f767f5af6ffb37c118acbbf6e86@o4510873826230272.ingest.us.sentry.io/4510873921912832",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
